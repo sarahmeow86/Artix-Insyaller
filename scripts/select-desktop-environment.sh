13037bed3e42064db8a871083e2a7ac38b4743c6
saretta1986 <saretta1986@proton.me>
2025-04-11 11:16:37 +0200

    Add desktop environment selection and corresponding package lists

--- scripts/select-desktop-environment.sh
+++ scripts/select-desktop-environment.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# filepath: /home/sarah/git-projects/Artix installer/select-desktop-environment.sh
 
 bold=$(tput setaf 2 bold)
 bolderror=$(tput setaf 3 bold)
@@ -18,12 +17,13 @@ fi
 
 # Prompt the user to select a desktop environment
 dialog --clear --title "Desktop Environment Selection" \
-    --menu "Choose a desktop environment to install:" 15 60 5 \
-    1 "Cinnamon" \
-    2 "MATE" \
-    3 "KDE Plasma (HIGHLY EXPERIMENTAL!)" \
-    4 "XFCE" \
-    5 "GNOME" 2> /tmp/de_choice
+    --menu "Choose a desktop environment to install:" 15 60 6 \
+    1 "Base (No Desktop Environment)" \
+    2 "Cinnamon" \
+    3 "MATE" \
+    4 "KDE Plasma (HIGHLY EXPERIMENTAL!)" \
+    5 "LXQt" \
+    6 "XFCE" 2> /tmp/de_choice
 
 # Read the user's choice
 DE_CHOICE=$(< /tmp/de_choice)
@@ -31,11 +31,12 @@ rm /tmp/de_choice
 
 # Map the choice to the corresponding pkglist file
 case $DE_CHOICE in
-    1) PKGLIST="pkglist-cinnamon.txt" ;;
-    2) PKGLIST="pkglist-mate.txt" ;;
-    3) PKGLIST="pkglist-kde.txt" ;;
-    4) PKGLIST="pkglist-xfce.txt" ;;
-    5) PKGLIST="pkglist-gnome.txt" ;;
+    1) PKGLIST="pkglist-base.txt" ;;
+    2) PKGLIST="pkglist-cinnamon.txt" ;;
+    3) PKGLIST="pkglist-mate.txt" ;;
+    4) PKGLIST="pkglist-plasma.txt" ;;
+    5) PKGLIST="pkglist-lxqt.txt" ;;
+    6) PKGLIST="pkglist-xfce.txt" ;;
     *) error "Invalid choice or no selection made!" ;;
 esac
 
