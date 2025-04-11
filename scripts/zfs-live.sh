41ee24344fb8ed01157a777eebf65b48a9ddbac9
saretta1986 <saretta1986@proton.me>
2025-04-11 11:49:47 +0200

    Add error handling and dialog installation check to ZFS live script

--- scripts/zfs-live.sh
+++ scripts/zfs-live.sh
@@ -1,3 +1,16 @@
+#!/usr/bin/env bash
+bold=$(tput setaf 2 bold)      # makes text bold and sets color to 2
+bolderror=$(tput setaf 3 bold) # makes text bold and sets color to 3
+normal=$(tput sgr0)            # resets text settings back to normal
+error() {\
+    printf "%s\n" "${bolderror}ERROR:${normal}\\n%s\\n" "$1" >&2; exit 1;
+}
+if ! command -v dialog &> /dev/null; then
+    echo "dialog is not installed. Installing it now..."
+    pacman -Sy --noconfirm dialog || { echo "Failed to install dialog. Please install it manually."; exit 1; }
+fi
+
+
 chaoticaur() {
     printf "%s\n" "## Installing Chaotic AUR ##"
 
