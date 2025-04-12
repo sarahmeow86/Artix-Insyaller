28ff1caf98d9935d35364562de9993e42eded8ac
saretta1986 <saretta1986@proton.me>
2025-04-12 09:32:42 +0200

    Fix path to pkglist.txt in installpkgs function and clean up formatting

--- scripts/installpkgs.sh
+++ scripts/installpkgs.sh
@@ -2,6 +2,8 @@
 bold=$(tput setaf 2 bold)      # makes text bold and sets color to 2
 bolderror=$(tput setaf 3 bold) # makes text bold and sets color to 3
 normal=$(tput sgr0)            # resets text settings back to normal
+
+
 error() {\
     printf "%s\n" "${bolderror}ERROR:${normal}\\n%s\\n" "$1" >&2; exit 1;
 }
@@ -10,6 +12,7 @@ if ! command -v dialog &> /dev/null; then
     pacman -Sy --noconfirm dialog || { echo "Failed to install dialog. Please install it manually."; exit 1; }
 fi
 
+
 installpkgs() {
     printf "%s\n" "${bold}Installing packages"
 
@@ -17,7 +20,7 @@ installpkgs() {
     (
         echo "10"; sleep 1
         echo "Installing base packages..."; sleep 1
-        basestrap $INST_MNT - < pkglist.txt && echo "50"
+        basestrap $INST_MNT - < misc/pkglist.txt && echo "50"
         echo "Installing kernel and ZFS packages..."; sleep 1
         basestrap $INST_MNT $INST_LINVAR ${INST_LINVAR}-headers linux-firmware zfs-dkms-git zfs-utils-git && echo "80"
         echo "Copying pacman configuration..."; sleep 1
